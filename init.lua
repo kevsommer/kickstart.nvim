b01b86c15d4e640d59657aeb4b82e026d9183da1
kevsommer <kevsommer96@gmail.com>
2025-02-27 12:34:05 +0100

    adds rose-pine theme

--- init.lua
+++ init.lua
@@ -885,8 +885,18 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000,
+    config = function()
+      vim.cmd 'colorscheme rose-pine'
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
+
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
@@ -903,7 +913,6 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 

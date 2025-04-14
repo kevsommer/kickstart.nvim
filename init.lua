2b7822787c5c210c5e5361fb1ac3f89c1a390868
kevsommer <kevsommer96@gmail.com>
2025-04-14 11:30:17 +0200

    adds vim fugitive

--- init.lua
+++ init.lua
@@ -119,6 +119,11 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  {
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'Gstatus', 'Gcommit', 'Gpush', 'Gpull' }, -- lazy-load on these commands
+    dependencies = { 'tpope/vim-rhubarb' }, -- optional (GitHub integration)
+  },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,

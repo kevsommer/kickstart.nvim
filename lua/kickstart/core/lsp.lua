f8198fa39e9ca2a462622bd0b7cadbda784321c1
kevsommer <kevsommer96@gmail.com>
2025-12-05 11:18:47 +0100

    fixes nerdfont signs issue

--- lua/kickstart/core/lsp.lua
+++ lua/kickstart/core/lsp.lua
@@ -207,14 +207,7 @@ return {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
         underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = vim.g.have_nerd_font and {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '€Z ',
-            [vim.diagnostic.severity.WARN] = '€* ',
-            [vim.diagnostic.severity.INFO] = '€ý ',
-            [vim.diagnostic.severity.HINT] = '€6 ',
-          },
-        } or {},
+        signs = {},
         virtual_text = {
           source = 'if_many',
           spacing = 2,

b77f177e7b1f0cf900d301ee1e4cefc39a0593cb
kevsommer <kevsommer96@gmail.com>
2025-12-05 15:20:47 +0100

    missing )

--- init.lua
+++ init.lua
@@ -592,7 +592,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
-}
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
